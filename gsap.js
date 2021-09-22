c55090de273f90d8828601e00f20d93e755b0734
Jason Nutt <jnutt367@gmail.com>
2021-09-21 22:30:48 -0400

    commit for last of the night

--- gsap.js
+++ gsap.js
@@ -1,8 +1,10 @@
 gsap.to(".cls-1", { fill: "orange" })
 gsap.to(".test", { x: 100, fill: "red", scale: 1.3 })
 gsap.fromTo(".the",{ y:0, opacity:0} ,{ y: 200, opacity:1, color:"black", duration: 2,ease:"elastic"})
-gsap.fromTo(".last",{ y:0, opacity:0} ,{ y: 200, x:200, fontSize:40,opacity:0.7, color:"black", fontFamily:"cursive", duration: 2,ease:"elastic"})
+gsap.fromTo(".last",{ y:0, opacity:0} ,{ y: 200, x:40, fontSize:40, opacity:0.7, color:"aqua", fontFamily:"fantasy", duration: 2,ease:"elastic"})
 gsap.fromTo(".ginger",{ y:400, opacity:0} ,{ y: 200, opacity:1, fontSize:45, duration:2, color:"red" , fontFamily:"fantasy",ease:"back.out"},3)
 gsap.fromTo(".bg-1", { opacity:0.5}, {opacity:1, fill:"orange" }, 5)
 gsap.fromTo(".bg-2", { opacity:0.5}, {opacity:1, fill:"orange" }, 5)
+gsap.to(".the", { paddingTop: 30, duration: 5,fontFamily:"fantasy"}, 5)
+
     
